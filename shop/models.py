3f433923d073154a6a1f2f9e31db8cb13c28dcae
Sajib <sajib35-1960@diu.edu.bd>
2021-03-25 20:09:45 +0600

    start

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    category_details = models.TextField(max_length=10000, null=True, blank=True)
+
+
+class SubCategory(models.Model):
+    sub_category_name = models.CharField(max_length=100)
+    sub_category_details = models.TextField(max_length=10000, null=True, blank=True)
+    parent_category = models.ForeignKey(Category, related_name='parent_category', on_delete=models.CASCADE, )
+
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=100)
+    brand_details = models.TextField(max_length=10000, null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    product_pic = models.ImageField(upload_to='pics/', null=True, blank=True)
+    product_details = models.TextField(max_length=10000, null=True, blank=True)
+    normal_price = models.IntegerField(max_length=100)
+    discount_price = models.IntegerField(max_length=100)
+    has_discount = models.BooleanField(default=False)
+    discount_amount = models.IntegerField(max_length=100, null=True, blank=True)
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE, )
+    sub_category = models.ForeignKey(SubCategory, related_name='sub_category', on_delete=models.CASCADE, )
+    brand = models.ForeignKey(Brand, related_name='brand', on_delete=models.CASCADE, )
