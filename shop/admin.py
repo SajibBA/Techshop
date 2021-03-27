3c41633e13ab57ed1e1303dd0a6d0e2b656d5298
Sajib <sajib35-1960@diu.edu.bd>
2021-03-27 23:35:07 +0600

    ongoing

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from shop.models import *
+
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Brand)
--- shop/models.py
+++ shop/models.py
@@ -7,16 +7,25 @@ class Category(models.Model):
     category_name = models.CharField(max_length=100)
     category_details = models.TextField(max_length=10000, null=True, blank=True)
 
+    def __str__(self):
+        return self.category_name
+
 
 class SubCategory(models.Model):
     sub_category_name = models.CharField(max_length=100)
     sub_category_details = models.TextField(max_length=10000, null=True, blank=True)
     parent_category = models.ForeignKey(Category, related_name='parent_category', on_delete=models.CASCADE, )
 
+    def __str__(self):
+        return self.sub_category_name
+
 
 class Brand(models.Model):
     brand_name = models.CharField(max_length=100)
     brand_details = models.TextField(max_length=10000, null=True, blank=True)
+
+    def __str__(self):
+        return self.brand_name
 
 
 class Product(models.Model):
@@ -30,3 +39,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE, )
     sub_category = models.ForeignKey(SubCategory, related_name='sub_category', on_delete=models.CASCADE, )
     brand = models.ForeignKey(Brand, related_name='brand', on_delete=models.CASCADE, )
+
+    def __str__(self):
+        return self.name
+
