c1a902031a60c6d483d85e7f0a7e8c3727e7bd68
Sajib <sajib35-1960@diu.edu.bd>
2021-03-28 00:34:35 +0600

    ongoing

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    product = Product.objects.all()
+    brand = Brand.objects.all()
+    category = Category.objects.all()
+    sub_category = SubCategory.objects.all()
+
+    context = {'product': product, 'brand': brand, 'category': category,
+               'sub_category': sub_category, }
+    return render(request, 'home.html', context)
