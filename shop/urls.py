9f19b7affaf06d6f027e216804275ebcde68e423
Sajib <sajib35-1960@diu.edu.bd>
2021-03-28 19:42:06 +0600

    ongoing

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path(r'view_sub_category_products/(?P<pk>\d+)/',
+         views.view_sub_category_products, name='view_sub_category_products'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import *
 
@@ -13,3 +14,20 @@ def home(request):
     context = {'product': product, 'brand': brand, 'category': category,
                'sub_category': sub_category, }
     return render(request, 'home.html', context)
+
+
+def view_sub_category_products(request, pk):
+    try:
+        subCategory = SubCategory.objects.get(pk=pk)
+    except SubCategory.DoesNotExist:
+        raise Http404
+    product = Product.objects.all()
+    brand = Brand.objects.all()
+    category = Category.objects.all()
+    sub_category = SubCategory.objects.all()
+    product_in_this = Product.objects.filter(sub_category=subCategory)
+
+    context = {'subCategory': subCategory, 'product': product,
+               'brand': brand, 'category': category,
+               'sub_category': sub_category, 'product_in_this': product_in_this}
+    return render(request, 'sub_category_products.html', context)
