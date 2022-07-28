a033cdae3f500150d3784bc18bf85355e53f4ad9
Sajib <sajib35-1960@diu.edu.bd>
2022-07-28 19:13:34 +0600

    Search funtionality added

--- shop/urls.py
+++ shop/urls.py
@@ -4,10 +4,12 @@ from .views import *
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('view_sub_category_products/<pk>/',
+     path('', views.home, name='home'),
+     path('view_sub_category_products/<pk>/',
          views.view_sub_category_products, name='view_sub_category_products'),
-    path('view_product_details/<pk>/',
+     path('view_product_details/<pk>/',
          views.view_product_details, name='view_product_details'),
+     path(r'^product$', searchproduct, name='searchproduct'),
+
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,12 @@
+from unicodedata import category
 from django.http import Http404
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
 
 # Create your views here.
 
+# Display all products.
 
 def home(request):
     product = Product.objects.all()
@@ -21,6 +24,7 @@ def home(request):
                'sub_category': sub_category, 'nonemty_sub_category' : nonemty_sub_category}
     return render(request, 'shop/home.html', context)
 
+# Show selected categorys products
 
 def view_sub_category_products(request, pk):
     try:
@@ -46,6 +50,9 @@ def view_sub_category_products(request, pk):
     return render(request, 'shop/sub_category_products.html', context)
 
 
+
+# Show details of the selected product
+
 def view_product_details(request, pk):
     try:
         product = Product.objects.get(pk=pk)
@@ -57,3 +64,39 @@ def view_product_details(request, pk):
     context = {'product': product, 'brand': brand, 'category': category,
                'sub_category': sub_category, }
     return render(request, 'shop/product_details.html', context)
+
+
+
+def searchproduct(request):
+    if request.method == 'GET':
+        query= request.GET.get('q')
+
+        submitbutton= request.GET.get('submit')
+
+        brand = Brand.objects.all()
+        product = Product.objects.all()
+        category = Category.objects.all()
+        sub_category = SubCategory.objects.all()
+        nonemty_sub_category = []
+        for sub_categorys in  sub_category:
+            for products in product:
+                if sub_categorys == products.sub_category:
+                    nonemty_sub_category.append(sub_categorys)
+                    break
+
+        if query is not None:
+            lookups= Q(name__icontains=query)
+
+            product= Product.objects.filter(lookups).order_by('category').distinct()
+
+            context={'product': product,
+                    'submitbutton': submitbutton, 'brand': brand, 'category': category,
+                    'sub_category': sub_category,'nonemty_sub_category' : nonemty_sub_category}
+
+            return render(request, 'shop/search.html', context)
+
+        else:
+            return render(request, 'shop/search.html')
+
+    else:
+        return render(request, 'shop/search.html')
