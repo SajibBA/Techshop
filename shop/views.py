622b0d83e8cbec6e4926d6304e6a42068155494a
SajibBA <sajibwrk@gmail.com>
2023-10-08 16:40:30 +0600

    added pagination

--- shop/views.py
+++ shop/views.py
@@ -5,26 +5,48 @@ from .models import *
 from django.db.models import Q
 from django.shortcuts import redirect, get_object_or_404, render
 from django.contrib import messages
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 # Display all products.
 
 def home(request):
-    product = Product.objects.all()
+    product = Product.objects.all().order_by("sub_category")
     brand = Brand.objects.all()
     category = Category.objects.all()
     sub_category = SubCategory.objects.all()
     nonemty_sub_category = []
-    for sub_categorys in  sub_category:
-        for products in product:
-            if sub_categorys == products.sub_category:
-                nonemty_sub_category.append(sub_categorys)
-                break
 
+
+    paginator = Paginator(product, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    page = request.GET.get('page', 1)
+    try:
+        product = paginator.page(page)
+        for sub_categorys in  sub_category:
+            for products in product:
+                if sub_categorys == products.sub_category:
+                    nonemty_sub_category.append(sub_categorys)
+                    break
+    except PageNotAnInteger:
+        product = paginator.page(1)
+        for sub_categorys in  sub_category:
+            for products in product:
+                if sub_categorys == products.sub_category:
+                    nonemty_sub_category.append(sub_categorys)
+                    break
+    except EmptyPage:
+        product = paginator.page(paginator.num_pages)
+        for sub_categorys in  sub_category:
+            for products in product:
+                if sub_categorys == products.sub_category:
+                    nonemty_sub_category.append(sub_categorys)
+                    break
     context = {'product': product, 'brand': brand, 'category': category,
-               'sub_category': sub_category, 'nonemty_sub_category' : nonemty_sub_category}
+               'sub_category': sub_category, 'nonemty_sub_category' : nonemty_sub_category,
+               "page_obj": page_obj}
     return render(request, 'shop/home.html', context)
 
 # Show selected categorys products
@@ -38,18 +60,43 @@ def view_sub_category_products(request, pk):
     brand = Brand.objects.all()
     category = Category.objects.all()
     sub_category = SubCategory.objects.all()
-    product_in_this = Product.objects.filter(sub_category=subCategory)
+    product_in_this = Product.objects.filter(sub_category=subCategory).order_by("brand")
+    
     brand_in_this = []
-    for brands in  brand:
-        for products in product_in_this:
-            if brands == products.brand:
-                brand_in_this.append(brands)
-                break
+    
+
+    paginator = Paginator(product_in_this, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    page = request.GET.get('page', 1)
+    try:
+        product_in_this = paginator.page(page)
+        for brands in  brand:
+            for products in product_in_this:
+                if brands == products.brand:
+                    brand_in_this.append(brands)
+                    break
+        
+    except PageNotAnInteger:
+        product_in_this = paginator.page(1)
+        for brands in  brand:
+            for products in product_in_this:
+                if brands == products.brand:
+                    brand_in_this.append(brands)
+                    break
+        
+    except EmptyPage:
+        product_in_this = paginator.page(paginator.num_pages)
+        for brands in  brand:
+            for products in product_in_this:
+                if brands == products.brand:
+                    brand_in_this.append(brands)
+                    break
 
     context = {'subCategory': subCategory, 'product': product,
                'brand': brand, 'category': category,
                'sub_category': sub_category, 'product_in_this': product_in_this,
-               'brand_in_this': brand_in_this, }
+               'brand_in_this': brand_in_this,"page_obj": page_obj }
     return render(request, 'shop/sub_category_products.html', context)
 
 
